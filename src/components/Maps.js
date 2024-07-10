fa20bf3d8fa42f073517b39e0beda3641950ca8a
SeanAverS <seanasuguitan@gmail.com>
2024-07-10 16:46:47 -0700

    Implement logic for fetching nearby businesses based on user location

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -3,6 +3,8 @@ import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 const Maps = ({ apiKey }) => {
   const [currentLocation, setCurrentLocation] = useState(null);
+  const [nearbyStores, setNearbyStores] = useState([]);
+  const [storesFetched, setStoresFetched] = useState(false);
 
   const mapContainerStyle = {
     width: "100%",
@@ -16,6 +18,26 @@ const Maps = ({ apiKey }) => {
     };
     console.log("Current location:", newLocation);
     setCurrentLocation(newLocation);
+    if (!storesFetched) {
+      fetchNearbyStores(newLocation);
+    }
+  }, [storesFetched]);
+
+  const fetchNearbyStores = useCallback((location) => {
+    const url = `http://localhost:5000/api/nearbyStores?lat=${location.lat}&lng=${location.lng}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.results) {
+          setNearbyStores(data.results);
+          setStoresFetched(true); 
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   }, []);
 
   useEffect(() => {
@@ -50,7 +72,7 @@ const Maps = ({ apiKey }) => {
   return (
     <GoogleMap
       mapContainerStyle={mapContainerStyle}
-      center={currentLocation} 
+      center={currentLocation}
       zoom={12}
     >
       {currentLocation && (
@@ -58,6 +80,15 @@ const Maps = ({ apiKey }) => {
           position={currentLocation}
         />
       )}
+      {nearbyStores.map((store, index) => (
+        <Marker
+          key={index}
+          position={{
+            lat: store.geometry.location.lat,
+            lng: store.geometry.location.lng,
+          }}
+        />
+      ))}
     </GoogleMap>
   );
 };
