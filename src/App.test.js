9807e9fb0af563250ecff8b7888f82ac94f182aa
SeanAverS <seanasuguitan@gmail.com>
2024-08-09 20:22:12 -0700

    Add map visibility tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Maps from './components/Maps';
+import { useJsApiLoader } from '@react-google-maps/api';
 
 jest.mock('@react-google-maps/api', () => ({
-  useJsApiLoader: jest.fn(() => ({
-    isLoaded: false,
-    loadError: null,
-  })),
+  useJsApiLoader: jest.fn(),
 }));
 
-test('renders loading message while maps are loading', () => {
-  render(<Maps />);
-  expect(screen.getByText('Loading.,..')).toBeInTheDocument();
-});
+describe('Maps Component', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
 
+  test('renders loading message while maps are loading', () => {
+    useJsApiLoader.mockReturnValue({ isLoaded: false, loadError: null });
+
+    render(<Maps />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  test('renders error message if there is a loadError', () => {
+    useJsApiLoader.mockReturnValue({ isLoaded: true, loadError: new Error('Load Error') });
+
+    render(<Maps />);
+    expect(screen.getByText('Error loading maps')).toBeInTheDocument();
+  });
+
+  test('renders the map when isLoaded is true and there is no loadError', () => {
+    useJsApiLoader.mockReturnValue({ isLoaded: true, loadError: null });
+
+    render(<Maps />);
+    expect(screen.getByRole('map')).toBeInTheDocument();  
+  });
+});
