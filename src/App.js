efeb4b04961e072d10100e442c1ee53819ef1c99
SeanAverS <seanasuguitan@gmail.com>
2025-04-02 17:59:26 -0700

    Remove uneeded api key variable

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Maps from './components/Maps.js';
 
 const App = () => {
-  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-
   return (
     <div>
       <h1>Guitar Store Locator</h1>
-      <p>Find guitar stores around your current location</p>
-      <Maps apiKey={apiKey} />
+      <p>Locate and get directions to guitar stores around you</p>
+      <Maps /> 
     </div>
   );
 };
