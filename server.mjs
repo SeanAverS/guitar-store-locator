01ae640f6b3be54ef4a255da80d01e538b777429
SeanAverS <seanasuguitan@gmail.com>
2025-04-03 16:54:17 -0700

    Add NodeCache to reduce data fetch repeats

--- server.mjs
+++ server.mjs
@@ -2,11 +2,13 @@ import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import NodeCache from 'node-cache';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cache = new NodeCache({ stdTTL: 600 }); 
 
 app.use(cors());
 
