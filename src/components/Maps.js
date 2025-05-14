07f915b90ce9a714d122f4b4f27a3c78e78db161
SeanAverS <seanasuguitan@gmail.com>
2025-05-13 18:56:55 -0700

    feature: state if store is open or closed inside info-window

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -99,6 +99,11 @@ const Maps = () => {
           </button>
           <h3>{activeMarker.name}</h3>
           <p>{activeMarker.vicinity || "No address available"}</p>
+          {activeMarker.opening_hours?.open_now !== undefined && (
+          <p>
+            <strong>Store Status:</strong>{" "}
+            {activeMarker.opening_hours.open_now ? "Open" : "Closed"}
+          </p> )}
           <a
             href={generateDirectionsUrl()}
             target="_blank"
