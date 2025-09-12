2bf5396f6bba8cfa80de61ecbf65e0b5b8e8d65e
SeanAverS <seanasuguitan@gmail.com>
2025-09-11 21:46:07 -0700

    chore: footer link to portfolio

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ const App = () => {
     <div>
       <h1>Guitar Store Locator</h1>
       <Maps />
+       <footer className="footer-container">
+        <p className="footer-text">
+          Coded by <a href="https://seanavers.github.io/portfolio/">Sean Suguitan</a>
+        </p>
+      </footer>
     </div>
   );
 };
